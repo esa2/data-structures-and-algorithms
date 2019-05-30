6e0f1ff5e648e0adbbeee46ecf6ec2a72b9693f4
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-05-30 09:16:50 -0700

    binary search method

--- code-challenges/src/test/java/code/challenges/ArrayShiftTest.java
+++ code-challenges/src/test/java/code/challenges/ArrayShiftTest.java
@@ -3,6 +3,7 @@ package code.challenges;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
 public class ArrayShiftTest {
     @Test public void insertShiftArray() {
         ArrayShift classUnderTest = new ArrayShift();
