192a3725b05d907900f94346842d2eaf4d14ff25
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-06-02 10:02:42 -0700

    add insert method

--- data-structures/src/main/java/data/structures/linkedlist/Linkedlist.java
+++ data-structures/src/main/java/data/structures/linkedlist/Linkedlist.java
@@ -1,4 +1,18 @@
 package data.structures.linkedlist;
 
 public class Linkedlist {
+    Node head;
+
+    public void insert(int val) {
+
+        Node node = new Node(val);
+
+        if (this.head == null) {
+            this.head = node;
+            node.next = null;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
 }
