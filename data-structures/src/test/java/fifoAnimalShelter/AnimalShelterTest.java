5aa93241b138d830439e1614ac52eb3cb06be5e1
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-06-12 11:45:20 -0700

    finish tests

--- data-structures/src/test/java/fifoAnimalShelter/AnimalShelterTest.java
+++ data-structures/src/test/java/fifoAnimalShelter/AnimalShelterTest.java
@@ -17,7 +17,6 @@ public class AnimalShelterTest {
 
     @Test
     public void testQueueClassNode() {
-
         assertNull("Should create empty queue", newAnimalShelter.dogFront);
     }
 
@@ -37,5 +36,48 @@ public class AnimalShelterTest {
         newCat.species = "cat";
         newAnimalShelter.enqueue(newCat);
         assertEquals("Should enqueue and dequeue a cat", newAnimalShelter.dequeue("cat"), newCat);
+    }
+
+    @Test
+    public void testQueueClassEnqueueMultipleDogs() {
+        Dog newDog1st = new Dog();
+        newDog1st.name = "Fido";
+        newDog1st.species = "dog";
+        newAnimalShelter.enqueue(newDog1st);
+
+        Dog newDog = new Dog();
+        newDog.name = "Cooter";
+        newDog.species = "dog";
+        newAnimalShelter.enqueue(newDog);
+        assertEquals("Should dequeue Fido first", newAnimalShelter.dequeue("dog"), newDog1st);
+    }
+
+    @Test
+    public void testQueueClassEnqueueMultipleAnimals() {
+        Dog newDog1st = new Dog();
+        newDog1st.name = "Fido";
+        newDog1st.species = "dog";
+        newAnimalShelter.enqueue(newDog1st);
+
+        Dog newDog = new Dog();
+        newDog.name = "Cooter";
+        newDog.species = "dog";
+        newAnimalShelter.enqueue(newDog);
+
+        Cat newCat1st = new Cat();
+        newCat1st.name = "Scratchy";
+        newCat1st.species = "cat";
+        newAnimalShelter.enqueue(newCat1st);
+
+        Cat newCat = new Cat();
+        newCat.name = "Itchy";
+        newCat.species = "cat";
+        newAnimalShelter.enqueue(newCat);
+        assertEquals("Should dequeue Scratchy first", newAnimalShelter.dequeue("cat"), newCat1st);
+    }
+
+    @Test
+    public void testQueueClassPrefInvalid() {
+        assertNull("Should return null in pref not dog or cat", newAnimalShelter.dequeue("horse"));
     }
 }
