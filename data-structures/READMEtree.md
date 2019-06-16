dff2fe7d6af646c1089ac17979a97be7f2affed5
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-06-16 14:17:45 -0700

    add bst

---
+++ data-structures/READMEtree.md
@@ -0,0 +1,18 @@
+## Lab Binary Tree and BST Implementation ##
+***
+
+**Node Class:**
+- Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
+
+**BinaryTree class:**
+- Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
+
+**BinarySearchTree class:**
+- Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+- Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
+
+**Approach & Efficiency**
+
+***
+
+[See the Node Class](src/main/java/tree/Node.java)
--- data-structures/src/test/java/tree/BinarySearchTreeTest.java
+++ data-structures/src/test/java/tree/BinarySearchTreeTest.java
@@ -1,5 +1,31 @@
 package tree;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BinarySearchTreeTest {
 
+    @Test
+    public void testBst() {
+        BinarySearchTree bst = new BinarySearchTree();
+        assertNull("Should instantiate an empty tree", bst.node);
+    }
+
+    @Test
+    public void testRootNode() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(10);
+        assertEquals("Should instantiate a tree with a single root node", bst.node.value, 10);
+    }
+
+    @Test
+    public void testLeftAndRightNode() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(10);
+        bst.add(5);
+        bst.add(15);
+        assertEquals("Should add a left child and right child to a single root node", bst.node.left.value, 5);
+        assertEquals("Should add a left child and right child to a single root node", bst.node.right.value, 15);
+    }
 }
