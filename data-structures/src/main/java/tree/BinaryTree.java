2b3baa27afe89b1770fd1b398925ad2439016329
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-06-17 09:53:09 -0700

    add preorder traversal

--- data-structures/src/main/java/tree/BinaryTree.java
+++ data-structures/src/main/java/tree/BinaryTree.java
@@ -1,11 +1,29 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTree {
+    Node node;
 
-    public int[] preOrder() {
+    public List preOrder(Node node) {
 
-        int[] arr = {1};
-        return arr;
+        ArrayList<Integer> list = new ArrayList<>();
+        Node root = node;
+
+        list.add(node.value);
+
+        while (node.left != null) {
+            list.add(node.left.value);
+            node = node.left;
+        }
+
+        node = root;
+        while (node.right != null) {
+            list.add(node.right.value);
+            node = node.right;
+        }
+        return list;
     }
 
     public int[] inOrder() {
