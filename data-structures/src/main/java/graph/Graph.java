e7444347cd77c15b30bd22dafafec8cafa5d8d11
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-07-15 20:10:20 -0700

    add methods

--- data-structures/src/main/java/graph/Graph.java
+++ data-structures/src/main/java/graph/Graph.java
@@ -1,13 +1,52 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Graph {
+    String name;
+    List<Edge> neighbors;
+    ArrayList<Graph> nodeList = new ArrayList<>();
 
-    public GraphNode addNode(String name) {
-        ArrayList<GraphNode> nodeList = new ArrayList<>();
-        GraphNode node = new GraphNode(name);
+    public Graph(String name) {
+        this.name = name;
+        this.neighbors = new LinkedList<>();
+    }
+
+    public Graph addNode(String name) {
+        Graph node = new Graph(name);
         nodeList.add(node);
         return node;
+    }
+
+    public void addEdge(Graph node1, Graph node2, int weight) {
+        if (!this.isConnectedTo(node2)) {
+            node1.neighbors.add(new Edge(weight, node2));
+            node2.neighbors.add(new Edge(weight, this));
+        }
+    }
+
+    public ArrayList getNodes() {
+        return nodeList;
+    }
+
+    public boolean isConnectedTo(Graph otherNode) {
+        for (Edge e : this.neighbors) {
+            if (e.node == otherNode) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class Edge {
+        int weight;
+        Graph node;
+
+        public Edge(int weight, Graph node) {
+            this.weight = weight;
+            this.node = node;
+        }
     }
 }
--- data-structures/src/test/java/graph/GraphTest.java
+++ data-structures/src/test/java/graph/GraphTest.java
@@ -8,9 +8,17 @@ public class GraphTest {
 
     @Test
     public void testAddNode() {
-        
-        Graph graph = new Graph();
-        GraphNode node = graph.addNode("test node");
+        Graph graph = new Graph("test");
+        Graph node = graph.addNode("test node");
         assertEquals("Should return a node named test node", graph.addNode("test node").name, node.name);
+    }
+
+    @Test
+    public void testAddEdge() {
+        Graph graph = new Graph("test");
+        Graph node1 = graph.addNode("node1");
+        Graph node2 = graph.addNode("node2");
+        graph.addEdge(node1, node2, 2);
+        assertEquals("Should return Edge with weight 2", node1.neighbors.get(0).weight, 2);
     }
 }
