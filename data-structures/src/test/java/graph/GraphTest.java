58e6159fe7f3b61c2e9c714b246e8b9109ac67b3
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-07-15 21:22:03 -0700

    add last tests

--- data-structures/src/test/java/graph/GraphTest.java
+++ data-structures/src/test/java/graph/GraphTest.java
@@ -2,6 +2,8 @@ package graph;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class GraphTest {
@@ -18,7 +20,56 @@ public class GraphTest {
         Graph graph = new Graph("test");
         Graph node1 = graph.addNode("node1");
         Graph node2 = graph.addNode("node2");
+        Graph node3 = graph.addNode("node3");
+        graph.addEdge(node1, node2, 2);
+        graph.addEdge(node1, node3, 2);
+        assertEquals("Should return Edge with weight 2", node1.neighbors.get(0).weight, 2);
+    }
+
+    @Test
+    public void testOneEdge() {
+        Graph graph = new Graph("test");
+        Graph node1 = graph.addNode("node1");
+        Graph node2 = graph.addNode("node2");
         graph.addEdge(node1, node2, 2);
         assertEquals("Should return Edge with weight 2", node1.neighbors.get(0).weight, 2);
+    }
+
+    @Test
+    public void testGetNodes() {
+        Graph graph = new Graph("test");
+        Graph node1 = graph.addNode("node1");
+        Graph node2 = graph.addNode("node2");
+        graph.addEdge(node1, node2, 2);
+        assertEquals("Should return node1 and node2 in a List", graph.getNodes().contains(node1), true);
+        assertEquals("Should return node1 and node2 in a List", graph.getNodes().contains(node2), true);
+    }
+
+    @Test
+    public void testGetNeighbors() {
+        Graph graph = new Graph("test");
+        Graph node1 = graph.addNode("node1");
+        Graph node2 = graph.addNode("node2");
+        graph.addEdge(node1, node2, 2);
+        Graph node3 = graph.addNode("node2");
+        graph.addEdge(node1, node3, 2);
+        List tt  = graph.getNeighbors(node1);
+        assertEquals("Should return node1 neighbor size of 2", graph.getNeighbors(node1).size(), 2);
+    }
+
+    @Test
+    public void testSize() {
+        Graph graph = new Graph("test");
+        Graph node1 = graph.addNode("node1");
+        Graph node2 = graph.addNode("node2");
+        Graph node3 = graph.addNode("node3");
+        Graph node4 = graph.addNode("node4");
+        assertEquals("Should return size of 4", graph.size(), 4);
+    }
+
+    @Test
+    public void testEmptyGraph() {
+        Graph graph = new Graph("test");
+        assertEquals("Should return null", graph.getNodes(), null);
     }
 }
